e2cf9d4cc43d88a95f30e1bb15554e5742577a45
Onion－Shen <18513385631@163.com>
2018-10-09 11:17:12 +0800

    fix bug

--- dataStructure/linkedList.py
+++ dataStructure/linkedList.py
@@ -26,32 +26,32 @@ class LinkedList(object):
 
     def insert_head(self, key: Any):
         node = Node(key)
+
         head = self.head()
         if not head:
             self.dummy.next = node
             node.prev = self.dummy
-        else:
-            prev = head.prev
+            return
+
+        prev = head.prev
+        if prev:
             prev.next = node
-            node.prev = prev
-            node.next = head
+        head.prev = node
+
+        node.prev, node.next = prev, head
 
     def head(self) -> Node:
         return self.dummy.next
 
     def search(self, key: Any) -> Node:
-        tmp = self.head()
-        while tmp:
-            if tmp.key == key:
-                return tmp
-            tmp = tmp.next
-        return None
+        head = self.head()
+        while head and head.key != key:
+            head = head.next
+        return head
 
     def delete(self, key: Any):
-        tmp = self.dummy
-        while tmp and tmp.key != key:
-            tmp = tmp.next
-        self.deleteNode(tmp)
+        node = self.search(key)
+        self.deleteNode(node)
 
     def deleteNode(self, node: Node):
         if not node:
@@ -67,31 +67,41 @@ class LinkedList(object):
         if node1 == node2:
             return
 
-        prev1, next1 = node1.prev, node1.next
-        prev2, next2 = node2.prev, node2.next
+        prev1, next1 = None, None
+        if node1:
+            prev1, next1 = node1.prev, node1.next
 
-        prev1.next = node2
-        node2.next = next1
+        prev2, next2 = None, None
+        if node2:
+            prev2, next2 = node2.prev, node2.next
 
-        prev2.next = node1
-        node1.next = next2
+        if node1:
+            node1.prev, node1.next = prev2, next2
 
-        node1.prev = prev2
-        node1.next = next2
+        if node2:
+            node2.prev, node2.next = prev1, next1
 
-        node2.prev = prev1
-        node2.next = next1
+        if prev1:
+            prev1.next = node2
+        if prev2:
+            prev2.next = node1
+
+        if next1:
+            next1.prev = node2
+        if next2:
+            next2.prev = node1
 
     def clear(self):
         self.dummy.next = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         head = self.head()
-        if head == self.dummy:
+        if not head:
             return None
 
         array = []
         while head:
-            array.append(head.key.__str__())
+            array.append(head.key)
             head = head.next
-        return ",".join(array)
+
+        return array.__str__()
