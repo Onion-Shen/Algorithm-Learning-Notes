77bd399b2b9c1d650d69fa96feec36569102d833
Onion－Shen <18513385631@163.com>
2018-10-24 14:28:42 +0800

    remove pycache before commit

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-#!/usr/local/bin/python3
+#!/usr/local/bin python3
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+
+def remove_pycache():
+    cwd = os.getcwd()
+    for dirpath, dirnames, _ in os.walk(cwd, followlinks=True):
+        for dirname in dirnames:
+            if dirname == "__pycache__":
+                pycache_dir_path = os.path.join(dirpath, dirname)
+                if os.path.isdir(pycache_dir_path):
+                    shutil.rmtree(pycache_dir_path)
 
 
 if __name__ == "__main__":
