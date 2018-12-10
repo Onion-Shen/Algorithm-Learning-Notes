b5cc80e70ee6ac029a814945dfbb8b745ace86d6
Onion－Shen <18513385631@163.com>
2018-12-10 19:01:41 +0800

    list graphy

--- dataStructure/graphy.py
+++ dataStructure/graphy.py
@@ -1,77 +1,23 @@
-from typing import List, Any, Dict
+from typing import List
 
 
-class ListGraphyNode(object):
-    def __init__(self, data: Any):
-        self.nextarc: ListGraphyNode = None
-        self.data: Any = data
-        self.visit = False
+class ArcNode(object):
+    def __init__(self, adjvex: int, nextarc: ArcNode):
+        self.adjvex = adjvex
+        self.nextarc = nextarc
 
-    def adjecent(self) -> List:
-        adj_array = []
-        head = self.nextarc
-        while head:
-            adj_array.append(head)
-            head = head.nextarc
-        return adj_array
+
+class VexNode(object):
+    def __init__(self, vertex: int, firstarc: ArcNode):
+        self.vertex = vertex
+        self.firstarc = firstarc
 
 
 class ListGraphy(object):
-    def __init__(self):
-        self.Adj: List[ListGraphyNode] = []
-
-    def vertexs(self) -> List[ListGraphyNode]:
-        vertex_array = []
-
-        for node in self.Adj:
-            while node:
-                vertex_array.append(node)
-                node = node.nextarc
-
-        return vertex_array
-
-
-class MatrixGraphy(object):
-    def __init__(self):
-        self.matrix: List[List[int]] = None
-
-
-def dfs(graphy: ListGraphy):
-    head = graphy.Adj[0]
-    print(head.data)
-    head.visit = True
-    stack = [head]
-    
-    while stack:
-        top = stack[-1]
-        idx = 0
-        adj_array = top.adjecent()
-
-        for v in adj_array:
-            if not v.visit:
-                print(v.data)
-                v.visit = True
-                stack.append(v)
-                break
-            idx += 1
-
-        if idx == len(adj_array):
-            stack.pop()
-
-
-def bfs(graphy: ListGraphy):
-    head = graphy.Adj[0]
-    isVisit: Dict[ListGraphyNode, bool] = {head: True}
-    queue: List[ListGraphyNode] = [head]
-    print(head.data)
-
-    while queue:
-        front = queue.pop(0)
-        adj_array = front.adjecent()
-
-        for node in adj_array:
-            visit = isVisit.get(node)
-            if not visit:
-                print(node.data)
-                queue.append(node)
-                isVisit[node] = True
+    def __init__(self, vexnum: int, arcnum: int):
+        self.adjlist: List[VexNode] = []
+        self.vexnum = vexnum
+        self.arcnum = arcnum
+        for i in range(vexnum):
+            head = VexNode(i, None)
+            self.adjlist.append(head)
